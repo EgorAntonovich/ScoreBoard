a07925942251f56e6672f5b842755ea9b6a605e8
EgorAntonovich <antonovich.egor1@gmail.com>
2024-03-24 19:49:21 +0100

    ScoreBoard. Add match service with interface and init match method.

---
+++ ScoreBoard/Services/Interfaces/IMatchService.cs
@@ -0,0 +1,6 @@
+namespace ScoreBoard.Services.Interfaces;
+
+public interface IMatchService
+{
+    public void InitMatch(string firstTeam, string secondTeam);
+}
---
+++ ScoreBoard/Services/MatchService.cs
@@ -0,0 +1,13 @@
+using ScoreBoard.Services.Interfaces;
+
+namespace ScoreBoard.Services;
+
+public class MatchService : IMatchService
+{
+    private 
+    
+    public void InitMatch(string firstTeam, string secondTeam)
+    {
+        throw new NotImplementedException();
+    }
+}
